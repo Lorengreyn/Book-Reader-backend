8915da5fa959584573d61df424235fecf9e9fc13
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-09-27 13:07:12 +0300

    добавлен коментарий что доделать

--- controllers/books/updateById.js
+++ controllers/books/updateById.js
@@ -1,21 +1,20 @@
 const { Book } = require('../../models/book');
 
-
 const { RequestError } = require('../../helpers');
 
 const updateById = async (req, res) => {
   const { id } = req.params;
   const { resume: review, readPages: pages } = req.body;
+
   const result = await Book.findById(id);
-  
 
   if (!result) {
     throw RequestError(404, 'Not found');
   }
 
   if (result.status === 'done') {
-    result.resume = review;}
-
+    result.resume = review;
+  }
 
   await result.save();
   res.json(result);
@@ -23,5 +22,4 @@ const updateById = async (req, res) => {
 
 module.exports = updateById;
 
-// Черновой вариант. логика работает только на одну книгу без проверки активна она или нет.
-// при статусе 'done' появляется доступ к отзыву
+//добавить проверку на владельца
