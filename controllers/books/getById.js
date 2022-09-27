822909223f7d68f9f3d3563d2c3e33b339b2c3eb
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-09-27 10:22:25 +0300

    add select in getById

--- controllers/books/getById.js
+++ controllers/books/getById.js
@@ -4,7 +4,12 @@ const { RequestError } = require('../../helpers');
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await Book.findById(id);
+  const result = await Book.findById(id).select({
+    _id: 0,
+    owner: 0,
+    createdAt: 0,
+    updatedAt: 0,
+  });
   //   const result = await Book.findOne({ id: id });
   if (!result) {
     throw RequestError(404, 'Not found');
