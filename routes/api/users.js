262750824bc36ad919b1107b5b9dce7ede6bb7cb
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-10-02 21:08:17 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -42,19 +42,20 @@ router.get(
 router.get(
   '/google/callback',
   passport.authenticate('google', {
-    successRedirect: '/api/users/success',
+    successRedirect: 'http://localhost:3000/book-reader-frontend/library',
   }),
   passport.authenticate('google', {
     failureRedirect: '/api/users/failed',
   }),
 );
 
-router.get('/library', (req, res) => {
-  res.send(`Welcome name - ${req.user.name},
-  id - ${req.user.id},
-  email - ${req.user.email},
-   `);
-});
+// router.get('/success', (req, res) => {
+//   // res.send(`Welcome name - ${req.user.name},
+//   // id - ${req.user.id},
+//   // email - ${req.user.email},
+//   //  `);
+//   successRedirect: 'http://localhost:3000/book-reader-frontend/library'
+// });
 
 router.get('/failed', (req, res) => {
   res.status(418).send(`Sorry something wrong`);
