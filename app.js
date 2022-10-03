b3e1489cf1955c1fa4a67317b303b7097c597be8
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-10-03 13:43:53 +0300

    1

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ const booksRouter = require('./routes/api/books');
 const trainingRouter = require('./routes/api/training');
 
 const app = express();
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200
-}
 
 app.use(
   cookieSession({
@@ -39,7 +35,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api/users',cors(corsOptions), authRouter);
+app.use('/api/users', authRouter);
 app.use('/api/books', booksRouter);
 app.use('/api/training', trainingRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
