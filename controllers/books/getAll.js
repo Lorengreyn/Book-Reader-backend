143febf1c236208e9dd2ece1a1c0b6f663bc0b4c
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-10-12 00:20:54 +0300

    fix pagination

--- controllers/books/getAll.js
+++ controllers/books/getAll.js
@@ -1,12 +1,14 @@
-const {Book} = require("../../models/book");
+const { Book } = require('../../models/book');
 
-const getAll = async(req, res) => {
-    const {_id: owner} = req.user;
-    const {page = 1, limit = 10} = req.query;
-    const skip = (page - 1) * limit;
-    const result = await Book.find({owner}, "-createdAt -updatedAt", {skip, limit: Number(limit)})
-                            .populate("owner", "email",);
-    res.json(result);
-}
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Book.find({ owner }, '-createdAt -updatedAt', {
+    skip,
+    limit: Number(limit),
+  }).populate('owner', 'email');
+  res.json(result);
+};
 
 module.exports = getAll;
